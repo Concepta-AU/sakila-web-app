5af7c9ad55a98dc32921a5f27e4bc076f3073979
Peter Becker <peter@concepta.au>
2026-04-04 09:20:29 +1000

    Move auth into module.

---
+++ server/src/main/kotlin/au/concepta/sakila/infra/LoginModule.kt
@@ -0,0 +1,53 @@
+package au.concepta.sakila.infra
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import kotlinx.serialization.Serializable
+import java.util.*
+
+fun Application.loginModule() {
+    val jwtSecret = environment.config.property("jwt.secret").getString()
+    val jwtIssuer = environment.config.property("jwt.issuer").getString()
+    val jwtAudience = environment.config.property("jwt.audience").getString()
+    val jwtRealm = environment.config.property("jwt.realm").getString()
+
+    authentication {
+        jwt {
+            realm = jwtRealm
+            verifier(
+                JWT
+                    .require(Algorithm.HMAC256(jwtSecret))
+                    .withAudience(jwtAudience)
+                    .withIssuer(jwtIssuer)
+                    .build()
+            )
+            validate { credential ->
+                if (credential.payload.audience.contains(jwtAudience)) JWTPrincipal(credential.payload) else null
+            }
+        }
+    }
+
+    routing {
+        post("/login") {
+            val user = call.receive<User>()
+            // Check username and password
+            // ...
+            val token = JWT.create()
+                .withAudience(jwtAudience)
+                .withIssuer(jwtIssuer)
+                .withClaim("username", user.username)
+                .withExpiresAt(Date(System.currentTimeMillis() + 60000))
+                .sign(Algorithm.HMAC256(jwtSecret))
+            call.respond(hashMapOf("token" to token))
+        }
+    }
+}
+
+@Serializable
+data class User(val username: String, val password: String)
