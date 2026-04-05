a322fbdb42c1c5de00e32d29fb5c5b1582f51dfb
Peter Becker <peter@concepta.au>
2026-04-05 18:02:28 +1000

    Basic login flow for client.

--- webApp/src/routes/+layout.svelte
+++ webApp/src/routes/+layout.svelte
@@ -1,7 +1,21 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
+	import { onMount, setContext } from 'svelte';
+	import { goto } from '$app/navigation';
 
 	let { children } = $props();
+	let user = $state<any>({});
+
+	setContext('user', {
+		get: () => user,
+		set: (newUser: any) => user = newUser
+	});
+
+	onMount(() => {
+		if (!user.token) {
+			goto('/login');
+		}
+	});
 </script>
 
 <svelte:head>
---
+++ webApp/src/routes/login/+page.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	import { getContext } from 'svelte';
+	import { goto } from '$app/navigation';
+
+	import Textfield from '@smui/textfield';
+	import Button, { Label } from '@smui/button';
+
+	let username = $state('');
+	let password = $state('');
+	let error = $state('');
+
+	let user = getContext<any>('user');
+
+	async function login() {
+		const response = await fetch('/api/login', {
+			method: 'POST',
+			body: JSON.stringify({ username, password }),
+			headers: { 'Content-Type': 'application/json' }
+		});
+		if (response.status !== 200) {
+			error = await response.text();
+		} else {
+			const result = await response.json();
+			user.set(result);
+			goto('/');
+		}
+	}
+</script>
+
+<h5>Login</h5>
+<div class="columns margins">
+	<div>
+		<Textfield bind:value={username} label="Username" />
+	</div>
+	<div>
+		<Textfield bind:value={password} label="Password" type="password" />
+	</div>
+	{#if error}
+		<div class="error">
+			{error}
+		</div>
+	{/if}
+	<Button onclick={login}>
+		<Label>Log in</Label>
+	</Button>
+</div>
+
+<style>
+    .error {
+        background: darkred;
+        color: whitesmoke;
+        margin: 1rem;
+        padding: 1rem;
+    }
+</style>
